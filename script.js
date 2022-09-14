2c5a8548051b0d04de8fd84305d7fc870eb6f7c6
Nurassyl Abdildabek <abdildabekn@mail.ru>
2022-09-15 00:15:44 +0600

    Пока что не закончен

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 const header = document.querySelector(".header");
+const select = document.getElementById("select");
+console.log(select);
 
 const getData = () => {
     return fetch("dbHeroes.json").then(res => res.json());
 }
 
 getData().then(users => {
-    
-
     users.forEach(elems => {
+        console.log(elems);
+        let realName = "";
+        if (elems.realName) {
+            realName = elems.realName;
+        }
         let cards = document.createElement('div');
-        let photo = "<img src= " + elems.photo  + "  alt=''>"
-        cards.innerHTML = photo;
-        header.append(cards)
+        cards.classList.add("block");
+        let photo = "";
+        select.addEventListener('input', () => {
+            if (select.options[select.selectedIndex].value == elems.status && select.options[select.selectedIndex].value == "deceased" ||
+                select.options[select.selectedIndex].value == elems.status && select.options[select.selectedIndex].value == "alive" ||
+                select.options[select.selectedIndex].value == elems.status && select.options[select.selectedIndex].value == "destroyed" ||
+                select.options[select.selectedIndex].value == elems.status && select.options[select.selectedIndex].value == "unknown") {
+                photo = "<img class='image' src= " +
+                    elems.photo +
+                    "  alt=''> <div class='elements'> <h3> Название: " + elems.name + "</h3> " +
+                    " <p> Актеры: " + realName + "</p> " +
+                    " <p> Спиок фильмов: " + elems.movies + " </p> " +
+                    " <p> Статус: " + elems.status + "</p> </div>";
+                cards.innerHTML = photo;
+                header.append(cards);
+            } else if (select.options[select.selectedIndex].value == "deceased" || select.options[select.selectedIndex].value == "alive" ||
+                select.options[select.selectedIndex].value == "destroyed" || select.options[select.selectedIndex].value == "unknown") {
+                photo = "";
+                cards.innerHTML = photo;
+                header.append(cards);
+            }
+        });
     });
-    
+
 })
